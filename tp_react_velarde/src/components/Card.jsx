a3280e317f127ca59ade62660bf37eb21999b3ad
marnvel <ricardom.velarde@gmail.com>
2023-04-11 12:16:43 -0300

    Se añadieron estilos para mejorar la presentación de las cards. 11-04-2023 12:16hs

--- tp_react_velarde/src/components/Card.jsx
+++ tp_react_velarde/src/components/Card.jsx
@@ -9,7 +9,7 @@ function Card({data, posicion, handleDelete}) {
 
     return (
         <div className='col-md-4 sm-6 d-flex justify-content-center align-items-center'>
-            <div className="card" style={ {width: "18rem"} }>
+            <div className="card mb-3" style={ {width: "18rem"} }>
                 <img src={data} className="card-img-top" alt="Imagen de perro" />
                 <div className="card-body">
                     <h5 className="card-title">Can</h5>
